031aac1e1e1ad37c95999448d67ef1ef5503b7a5
Manuel Ribeiro <61391856+ManuelRibeiro89247@users.noreply.github.com>
2024-02-02 03:07:24 +0000

    original files

---
+++ Aula 5/Assets/Teletransport.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teletransport : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject reticle;
+    public Color targetColor;
+    private Color initial;
+    private float timer;
+    private bool hasFocus = false;
+    private MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        hasFocus = false;
+        meshRenderer=this.GetComponent<MeshRenderer>();
+        initial = meshRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasFocus)
+        {
+            meshRenderer.material.color = Color.Lerp(meshRenderer.material.color, targetColor,Time.deltaTime/2f);
+            timer += Time.deltaTime;
+            if (timer > 2)
+            {
+                player.transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void teletransport()
+    {
+        player.transform.position = new Vector3(transform.position.x,player.transform.position.y,transform.position.z);
+        //Destroy(gameObject);
+        gameObject.SetActive(false);
+    }
+
+    public void teletransportTime(bool selected)
+    {
+        hasFocus = selected;
+        if (!hasFocus)
+        {
+            timer = 0;
+            meshRenderer.material.color = initial;
+        }
+    }
+
+}
